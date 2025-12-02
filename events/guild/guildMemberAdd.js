46f0d7cc4c85d9b15a5b5f7c84af5bec8534b434
Seyit Mehmet <seyit.mehmet.aladag42@gmail.com>
2025-12-02 19:48:41 +0300

    fix bug

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -12,6 +12,7 @@ module.exports = {
             const channel = await member.guild.channels.fetch(welcomeChannelID);
             if (!channel) return;
 
+            // v5 Uyumlu Güvenli Ayarlar
             const card = new Welcomer()
                 .setUsername(member.user.username)
                 .setDiscriminator(member.user.discriminator === '0' ? ' ' : member.user.discriminator)
@@ -19,30 +20,27 @@ module.exports = {
                 .setGuildName(member.guild.name)
                 .setAvatar(member.user.displayAvatarURL({ extension: 'png', forceStatic: true }))
                 
-                // --- RENK AYARLARI (Temiz Görünüm) ---
-                .setColor("title", "#FF5500")        // Başlık (HOŞGELDİN) Turuncu
-                .setColor("username-box", "transparent") // Kutu YOK
-                .setColor("discriminator-box", "transparent")
-                .setColor("message-box", "transparent") // Kutu YOK
-                .setColor("border", "#FF5500")       // Avatar Kenarlığı Turuncu
+                // STANDART RENKLER (Çökmemesi için)
+                .setColor("title", "#FF5500") 
+                .setColor("username-box", "#000000") // Siyah Kutu (Şimdilik)
+                .setColor("discriminator-box", "#000000")
+                .setColor("message-box", "#000000")
+                .setColor("border", "#FF5500")
                 .setColor("avatar", "#FF5500")
                 
-                // --- YAZI AYARLARI ---
-                .setText("title", "HOŞGELDİN") 
-                .setText("message", "AVELLERE KATILDI") 
-                .setText("member-count", "- Toplam Üye: {count} -")
+                .setText("title", "HOŞGELDİN")
+                .setText("message", "SUNUCUYA KATILDI")
+                .setText("member-count", "- Üye Sayısı: {count} -")
                 
-                // --- ARKA PLAN (Koyu Turuncu/Siyah) ---
                 .setBackground("https://wallpapers.com/images/hd/black-and-orange-background-1920-x-1080-4i32732950669273.jpg");
 
             const buffer = await card.build();
             const attachment = new AttachmentBuilder(buffer, { name: 'welcome.png' });
             
-            // Mesaj içeriği
             await channel.send({ content: `${member} aramıza katıldı!`, files: [attachment] });
 
         } catch (err) {
-            console.log("[HATA] Resim oluşturulamadı:", err);
+            console.log("[HATA] Resim hatası:", err);
         }
     }
 };
